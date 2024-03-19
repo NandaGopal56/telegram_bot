5afdc0773e4f3e2f9bac4b0ad64c4b8cf00b3475
Nanda Gopal Pattanayak <40311776+NandaGopal56@users.noreply.github.com>
2024-03-19 23:12:07 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,74 @@
+import os
+import telebot
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Hey! how are you doing?")
+
+
+@bot.message_handler(commands=['horoscope'])
+def sign_handler(message):
+    text = '''
+    What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*.
+
+    <b>bold</b>
+    <strong>bold</strong>
+    <i>italic</i>
+    <em>italic</em>
+    <u>underline</u>
+    <ins>underline</ins>
+    <s>strikethrough</s>
+    <strike>strikethrough</strike>
+    <del>strikethrough</del>
+    <span class="tg-spoiler">spoiler</span>
+    <tg-spoiler>spoiler</tg-spoiler>
+    <b>bold <i>italic bold <s>italic bold strikethrough <span class="tg-spoiler">italic bold strikethrough spoiler</span></s> <u>underline italic bold</u></i> bold</b>
+    <a href="http://www.example.com/">inline URL</a>
+    <a href="tg://user?id=123456789">inline mention of a user</a>
+    '''
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="html")
+    bot.register_next_step_handler(sent_msg, day_handler)
+
+
+def day_handler(message):
+    sign = message.text
+    text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
+    sent_msg = bot.send_message(
+        message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(
+        sent_msg, fetch_horoscope, sign.capitalize())
+
+
+def get_daily_horoscope(sign: str, day: str) -> dict:
+    """Get daily horoscope for a zodiac sign.
+    Keyword arguments:
+    sign:str - Zodiac sign
+    day:str - Date in format (YYYY-MM-DD) OR TODAY OR TOMORROW OR YESTERDAY
+    Return:dict - JSON data
+    """
+    url = "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily"
+    params = {"sign": sign, "day": day}
+    response = requests.get(url, params)
+
+    return response.json()
+
+
+def fetch_horoscope(message, sign):
+    day = message.text
+    horoscope = get_daily_horoscope(sign, day)
+    data = horoscope["data"]
+    horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\\n*Sign:* {sign}\\n*Day:* {data["date"]}'
+    bot.send_message(message.chat.id, "Here's your horoscope!")
+    bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
+
+
+bot.infinity_polling()
